5af3f305cd4a9eeafaefea094aef3a6b7683cc0d
Ngandu Ntita <100237894+nganduntita1@users.noreply.github.com>
2025-04-21 14:11:49 +0200

    fixed

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,7 +5,7 @@ import { Separator } from "@/components/ui/separator";
 import AnnouncementBar from "../components/AnnouncementBar";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-import img1 from '../assets/1.JPG';
+import img1 from '../assets/1.jpg';
 
 import img2 from "../assets/img2.JPG";
 import img3 from "../assets/img3.jpeg";
